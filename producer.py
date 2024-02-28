93a628163faf58113597e1fe49e4962cb15f12f9
shreyasskasetty-tamu <shreyasskasetty@tamu.edu>
2024-02-28 00:01:33 -0600

    add kafka basic producer, consumer
    Implemented producer and consumer with different number of partitions

---
+++ producer.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+from data import get_registered_user
+from time import sleep
+
+import json
+
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+def get_partitioner(key_bytes, all_partition, available_partition):
+    return 0
+
+producer  = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                          value_serializer=json_serializer,
+                          partitioner=get_partitioner)
+
+if __name__ == "__main__":
+    while 1:
+        registered_user = get_registered_user()
+        print(registered_user)
+        producer.send("registered_user", registered_user)
+        sleep(4)
